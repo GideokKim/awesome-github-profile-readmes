378f2b7123088e251f8d7cac61e1a143a53eb40d
gideok Kim <ikid4127@naver.com>
2025-04-22 00:13:41 +0900

    🚀 Deploy pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/awesome-github-profile-readmes',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
